2bbf13a674386fec8245010ff21a21e3b107cabd
Ochoa-Carlos <carlos@digammacsi.com.mx>
2025-01-13 09:09:46 -0700

    add: constants

--- constants.py
+++ constants.py
@@ -6,3 +6,7 @@ caracteres = {
     "permisionario": ["ModalidadPermiso", "NumPermiso"],
     "usuario": ["InstalacionAlmacenGasNatural"]
 }
+
+VERSION_REGEX = r"[0-9]+\.[0-9]+$"
+MODALITY_PERMISSION_REGEX = r"^PER([1-9]|[1-4][0-9]|5[0-6])$"
+UTC_FORMAT_REGEX = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}:\d{2}$"
