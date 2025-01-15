f34861d6c88c1fe2f27dc58347e8d8a9660f8936
Ochoa-Carlos <carlos@digammacsi.com.mx>
2025-01-15 15:48:32 -0700

    feat: decorator to handle exceptions on internal validators classes

--- decorators.py
+++ decorators.py
@@ -11,9 +11,31 @@ def wrapper_handler(func):
             self.executed_functions.add(func.__name__)
             return result
         except Exception as err:
-            print(f"Excepción atrapada en {func.__name__}: {err}")
+            # print(f"Excepción atrapada en {func.__name__}: {err}")
             self.executed_functions.add(func.__name__)
-            self.error[func.__name__] = err
-            self.errors.append(err)
+            self.errors[func.__name__] = err
+            self.error.append(err)
             return None
+    return wrapper
+
+
+# TODO redistribuir y unir de ser posible ambos decoradores
+def exception_wrapper(func):
+    """Exception wrapper for internals classes ."""
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs) -> None:
+        if hasattr(self, "_executed_functions") and func.__name__ in self.exc_funcs:
+            return
+        try:
+            result = func(self, *args, **kwargs)
+            self.exc_funcs = func.__name__
+            return result
+        except Exception as err:
+            # print(f"Excepción atrapada en {func.__name__}: {err}")
+            err_obj = {
+                "func_error": func.__name__,
+                "error": str(err)
+            }
+            self.errors = err_obj
+            return
     return wrapper
