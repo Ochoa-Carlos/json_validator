e4fe618b21c07c4f10f564807f6c74ca88d72aa1
Ochoa-Carlos <carlos@digammacsi.com.mx>
2025-01-20 15:57:10 -0700

    add

---
+++ complements/__init__.py
@@ -0,0 +1,3 @@
+from .storage_complement import StorageComplement
+from .comercialization_complement import ComercializationComplement
+from .complement_base import ComplementBuilder
---
+++ complements/helpers.py
@@ -0,0 +1,16 @@
+from typing import TypeVar
+from complements import StorageComplement, ComplementBuilder, ComercializationComplement
+
+
+ComplementType = TypeVar("ComplementType", bound="ComplementBuilder")
+
+
+def complement_builder(complement_data: dict, complement_type: str) -> ComplementType:
+    print("INSTANCIANDO COMPLEMETNO")
+    complement_map = {
+        "Almacenamiento": StorageComplement,
+        "Comercializacion": ComercializationComplement,
+    }
+
+    complement_class = complement_map.get(complement_type)
+    return complement_class(complement_dict=complement_data, complement_type=complement_type)
