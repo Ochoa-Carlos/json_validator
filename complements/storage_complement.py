9e8d61ea2c839af1d79a82de4008caf1775353fb
Ochoa-Carlos <carlos@digammacsi.com.mx>
2025-01-22 16:38:05 -0700

    add: almacenamiento complement type class

---
+++ complements/storage_complement.py
@@ -0,0 +1,15 @@
+from complements.complement_base import ComplementBuilder
+from dict_types import complement_transport
+from dict_type_validator import DictionaryTypeValidator
+
+
+class StorageComplement(ComplementBuilder):
+    """Complement for comercialization user type."""
+    # def validate_complemento(self) -> None:
+    #     print("INSTANCIA DE STORAGE")
+    #     self._validate_complemento_tipado()
+
+    # def _validate_complemento_tipado(self) -> None:
+    #     pass
+    # def _validate_transporte(self) -> None:
+    #     print("ValidateComplemento DESDE MI STORAGE CLASS")
