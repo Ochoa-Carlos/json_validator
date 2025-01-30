13bac904d7def2934751e17402f3d11238e58556
Ochoa-Carlos <carlos@digammacsi.com.mx>
2025-01-30 14:24:17 -0700

    ?????

---
+++ exceptions/__init__.py
@@ -0,0 +1,7 @@
+from .custom_exceptions import BaseError, RegexError, ErrorPrueba
+
+__all__ = [
+    # RegexError.__name__,
+    "ErrorPrueba",
+    "BaseError"
+]
---
+++ exceptions/custom_exceptions.py
@@ -0,0 +1,54 @@
+
+
+class BaseError(Exception):
+    """Base class for custom exception"""
+    def __init__(self, message):
+        super().__init__(message)
+
+class RegexError(BaseError):
+    """Custom Regex Error."""
+
+class CaracterError(BaseError):
+    """Custom Contratista Error."""
+
+class CaracterContratistaError(BaseError):
+    """Custom Contratista Error."""
+
+class CaracterAsignatarioError(BaseError):
+    """Custom Asignatario Error."""
+
+class CaracterPermisionarioError(BaseError):
+    """Custom Permisionario Error."""
+
+class CaracterUsuarioError(BaseError):
+    """Custom Usuario Error."""
+
+class LongitudError(BaseError):
+    """Custom length error."""
+
+class ValorMinMaxError(BaseError):
+    """Custom value range error."""
+
+class RequiredError(BaseError):
+    """Custom length error."""
+
+class ClaveSubProductoError(BaseError):
+    """Custom clave sub producto error."""
+
+class ClaveProductoError(BaseError):
+    """Custom clave sub producto error."""
+
+class RecepcionesError(BaseError):
+    """Custom reception error."""
+
+class ControlExistenciasError(BaseError):
+    """Custom reception error."""
+
+class EntregasError(BaseError):
+    """Custom reception error."""
+
+class BitacoraMensualError(BaseError):
+    """Custom reception error."""
+
+class ErrorPrueba(BaseError):
+    """Custom reception error."""
