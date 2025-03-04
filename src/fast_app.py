6fe052fd891a7b9dab7bfcaece0e26c42a3c664a
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-03-04 11:43:13 -0700

    style: pep changes

--- src/fast_app.py
+++ src/fast_app.py
@@ -1,21 +1,23 @@
 import json
-from fastapi import FastAPI, UploadFile, File
+
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+
 from src.json_validator import JsonValidator
 
 app = FastAPI()
 
-# Montar la carpeta 'static' para servir archivos estáticos
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    # Servir el archivo index.html
-    return open("src/static/index.html").read()
+    """Render json validator."""
+    return open("src/static/index.html", "rb").read()
 
 @app.post("/upload/")
 async def upload_json(file: UploadFile = File(...)):
+    """Upload json file endp."""
     try:
         # Leemos el archivo JSON subido
         content = await file.read()
@@ -38,9 +40,11 @@ async def upload_json(file: UploadFile = File(...)):
 
         # Formatear el JSON para enviarlo con indentación
         formatted_json = json.dumps(json_data, indent=4)
-        print("LISTA DE ERRORES", error_list)
+
         # Devolver el JSON formateado y los errores (si los hay)
         return {"json_data": formatted_json, "errors": error_list}
 
     except json.JSONDecodeError as e:
-        return {"error": f"Error al cargar el archivo: {str(e)}"}
+        return {"Error": f"Error al cargar el archivo: {str(e)}"}
+    except Exception as exc:
+        return {"Error": f"Error al cargar el archivo: {str(exc)}"}
