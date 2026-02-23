cb9902990021c63d2c79c6504264919ed3e120ad
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2026-02-23 08:56:27 -0700

    add: json month schema

--- src/constants.py
+++ src/constants.py
@@ -28,6 +28,30 @@ products_keys = {
 subproducts_keys = ["PR03", "PR07", "PR08", "PR09", "PR11", "PR13", "PR15", "PR16", "PR17", "PR18", "PR19"]
 petroleo_caracteres = ["contratista", "asignatario"]
 cal_value_caracteres = ["contratista", "asignatario"]
+monthly_json_schema = [
+    "Version",
+    "RfcContribuyente",
+    "RfcRepresentanteLegal",
+    "RfcProveedor",
+    "RfcProveedores",
+    "Caracter",
+    "ModalidadPermiso",
+    "NumPermiso",
+    "NumContratoOAsignacion",
+    "InstalacionAlmacenGasNatural",
+    "ClaveInstalacion",
+    "DescripcionInstalacion",
+    "Geolocalizacion",
+    "NumeroPozos",
+    "NumeroTanques",
+    "NumeroDuctosEntradaSalida",
+    "NumeroDuctosTransporteDistribucion",
+    "NumeroDispensarios",
+    "FechaYHoraReporteMes",
+    "Producto",
+    "BitacoraMensual"
+    ]
+
 event_type = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21]
 component_alarm = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 
@@ -39,3 +63,4 @@ SUBPRODUCTO_REGEX = r"^SP([1-9]|[1-3][0-9]|4[0-8])$"
 CONDENSEDGAS_REGEX = r"^GNC(0[1-9]|10)$"
 RFC_PERSONA_FISICA = r"^([A-ZÑ]|\&){4}[0-9]{2}(0[1-9]|1[0-2])([12][0 -9]|0[1-9]|3[01])[A-Z0-9]{3}$"
 FILE_NAME_REGEX = r"^M_([A-Za-z0-9]{8}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12})_[A-Z0-9Ñ&]{12,13}_[A-Z0-9Ñ&]{12}_(\d{4}-\d{2}-\d{2})_(ACA|AUP|ALM|ACO|BSP|BDE|CMN|COM|CON|DEN|DIS|EMA|ESN|EDS|ESA|EXO|EXP|EXT|GSH|LON|RPO|PTA|PDD|PGN|RCN|REF|RGN|SIS|SFO|SDA|TDA|TDD|TRA|TDP|USP|ACL|ASN|MNA|TRE)-\d{4}_(EXT|REF|PGN|CON|DEN|LON|RGN|TRA|ALM|AGA|USP|DIS|CMN|EXO)_JSON$"
+# FILE_NAME_REGEX = r"^M_[A-Za-z0-9]{36}_[A-ZÑ&]{3,4}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])[A-Z0-9]{3}_[A-ZÑ&]{3}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])[A-Z0-9]{3}_[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])_((ACA|AUP|ALM|ACO|BSP|BDE|CMN|COM|CON|DEN|DIS|EMA|ESN|EDS|ESA|EXO|EXP|EXT|GSH|LON|RPO|PTA|PDD|PGN|RCN|REF|RGN|SIS|SFO|SDA|TDA|TDD|TRA|TDP|USP)-[0-9]{4}|(ACL|ASN)-(MNA|TRE)-[A-Z0-9]{4})_(EXT|REF|PGN|CON|DEN|LON|RGN|TRA|ALM|AGA|USP|DIS|CMN|EXO)_JSON\.zip$"
